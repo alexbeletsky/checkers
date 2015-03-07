ee4e624b506b10ec423ddcd9b638ed6866577e91
alexanderbeletsky <alexander.beletsky@gmail.com>
2015-03-07 10:24:50 +0100

    check_fb - use dynamic rate limit

--- check_fb.js
+++ check_fb.js
@@ -3,7 +3,12 @@ var request = require('request');
 var url = 'https://likeastore.com';
 
 function check(url, callback) {
-    callback(null, {resp: {}, headers: {}});
+    var apiUrl = 'https://api.facebook.com/method/links.getStats?format=json&urls=' + url;
+
+    request({url: apiUrl, json: true}, function (err, resp, body) {
+        callback(err, {resp: body, headers: resp.headers});
+    });
+
 }
 
 check(url, function (err, resp) {
